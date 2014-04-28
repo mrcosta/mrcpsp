78f84f4ff821ae19eafa3d56d92cbd0f5ca0cd08
Mr Costa <mateuscomp@gmail.com>
2014-04-28 00:30:47 -0300

    added the makespan values for the two file that are been compared

--- src/main/groovy/mrcpsp/process/JobTimeProcessor.groovy
+++ src/main/groovy/mrcpsp/process/JobTimeProcessor.groovy
@@ -12,7 +12,7 @@ import org.apache.log4j.Logger
 
 class JobTimeProcessor {
 
-	static final Logger log = Logger.getLogger(JobTimeProcessor.class);
+    static final Logger log = Logger.getLogger(JobTimeProcessor.class);
 
     ModeOperations modeOperations
     JobPriorityRulesOperations jobPriorityRulesOperations
@@ -24,47 +24,55 @@ class JobTimeProcessor {
         jobOperations = new JobOperations()
     }
 
-	List<Job> getJobTimes(ResourceAvailabilities ra, List<Job> jobs) {
-		boolean checkResources = false
+    List<Job> getJobTimes(ResourceAvailabilities ra, List<Job> jobs) {
+        boolean checkResources = false
 
         ra.resetRenewableResources()
         jobs*.startTime = 0
         jobs*.endTime = 0
 
-		jobs.each { job ->
-			log.debug("Getting start and finish time - JOB: $job.id")
+        jobs.each { job ->
+            log.debug("Getting start and finish time - JOB: $job.id")
 
-			if (job.predecessors.isEmpty()) {
-				checkResources = setTimeJobWithoutPredecessors(ra, job, jobs)
-			} else {				
-				checkResources = setTimeJobWithPredecessors(ra, job, jobs);
-			}
-		}
-		
-		return jobs;
-	}
-	
-	Job setTimeJobWithoutPredecessors(ResourceAvailabilities ra, Job jobToSchedule, List<Job> jobs) {
+            if (job.predecessors.isEmpty()) {
+                checkResources = setTimeJobWithoutPredecessors(ra, job, jobs)
+            } else {
+                checkResources = setTimeJobWithPredecessors(ra, job, jobs);
+            }
+        }
+
+        return jobs;
+    }
+
+    Job setTimeJobWithoutPredecessors(ResourceAvailabilities ra, Job jobToSchedule, List<Job> jobs) {
         jobToSchedule.startTime = 0
         jobToSchedule.endTime = jobToSchedule.startTime + jobToSchedule.mode.duration
 
-        //get all the jobs that was already scheduled
-        ra.scheduledJobs = jobOperations.getJobsBetweenInterval(jobToSchedule, jobs)
+        def isSchedulled = false
+        while (!isSchedulled) {
+            //get all the jobs that was already scheduled in the proposed interval for the job that will be scheduled
+            ra.scheduledJobs = jobOperations.getJobsBetweenInterval(jobToSchedule, jobs)
 
-        ra.scheduledJobs?.each {
-            modeOperations.addingRenewableResources(ra, it.mode)
-        }
+            ra.scheduledJobs?.each {
+                modeOperations.addingRenewableResources(ra, it.mode)
+            }
 
-        log.info("Scheduling job $jobToSchedule.id - Mode $jobToSchedule.mode.id RR: $jobToSchedule.mode.renewable - Jobs in interval: " + ra.scheduledJobs?.id + " - Resources usage: " + ra.toStringRenewable())
+            log.info("Scheduling job without predecessors - $jobToSchedule.id - Mode $jobToSchedule.mode.id RR: $jobToSchedule.mode.renewable - Jobs in interval: " + ra.scheduledJobs?.id + " - Resources usage: " + ra.toStringRenewable())
 
-        // then we need to remove the jobs to schedule the one that we want and change its time (start and end)
-        if (!modeOperations.checkRenewableResourcesAmount(ra, jobToSchedule.mode)) {
-            jobToSchedule = setJobTimeUsingScheduledJobs(ra, jobToSchedule)
+            // then we need to remove the jobs to schedule the one that we want and change its time (start and end)
+            if (!modeOperations.checkRenewableResourcesAmount(ra, jobToSchedule.mode)) {
+                jobToSchedule = setJobTimeUsingScheduledJobs(ra, jobToSchedule)
+            } else {
+                isSchedulled = true
+            }
+
+            resetResources(ra)
+            ra.scheduledJobs.clear()
         }
 
         log.info("JOB $jobToSchedule.id was scheduled: {start: $jobToSchedule.startTime, end: $jobToSchedule.endTime}\n")
         return jobToSchedule
-	}
+    }
 
     Job setTimeJobWithPredecessors(ResourceAvailabilities ra, Job jobToSchedule, List<Job> jobs) {
         List<Job> jobPredecessors = jobOperations.getJobPredecessors(jobToSchedule, jobs)
@@ -74,20 +82,24 @@ class JobTimeProcessor {
         jobToSchedule.startTime = latestJob.endTime
         jobToSchedule.endTime = jobToSchedule.startTime + jobToSchedule.mode.duration
 
-        //get all the jobs that was already scheduled
-        ra.scheduledJobs = jobOperations.getJobsBetweenInterval(jobToSchedule, jobs)
+        def isSchedulled = false
+        while (!isSchedulled) {
+            //get all the jobs that was already scheduled in the proposed interval for the job that will be scheduled
+            ra.scheduledJobs = jobOperations.getJobsBetweenInterval(jobToSchedule, jobs)
 
-        ra.scheduledJobs?.each {
-            modeOperations.addingRenewableResources(ra, it.mode)
-        }
+            ra.scheduledJobs?.each {
+                modeOperations.addingRenewableResources(ra, it.mode)
+            }
 
-        log.info("Scheduling job $jobToSchedule.id - Mode $jobToSchedule.mode.id RR: $jobToSchedule.mode.renewable - Jobs in interval: " + ra.scheduledJobs?.id + " - Resources usage: " + ra.toStringRenewable())
+            log.info("Scheduling job with predecessors - $jobToSchedule.id - Mode $jobToSchedule.mode.id RR: $jobToSchedule.mode.renewable - Jobs in interval: " + ra.scheduledJobs?.id + " - Resources usage: " + ra.toStringRenewable())
 
-        // we need to remove the jobs to schedule the one that we want and change its time (start and end)
-        if (!modeOperations.checkRenewableResourcesAmount(ra, jobToSchedule.mode)) {
-            jobToSchedule = setJobTimeUsingScheduledJobs(ra, jobToSchedule)
-        } else {
-            // just reseting the resources and cleaning the list
+            // we need to remove the jobs to schedule the one that we want and change its time (start and end)
+            if (!modeOperations.checkRenewableResourcesAmount(ra, jobToSchedule.mode)) {
+                jobToSchedule = setJobTimeUsingScheduledJobs(ra, jobToSchedule)
+            } else {
+                isSchedulled = true
+            }
+
             resetResources(ra)
             ra.scheduledJobs.clear()
         }
@@ -114,9 +126,6 @@ class JobTimeProcessor {
 
             ra.scheduledJobs.remove(0)
         }
-
-        resetResources(ra)
-        ra.scheduledJobs.clear()
 
         return jobToSchedule
     }
